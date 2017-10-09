01d21c03bbbd64e69fe9d831b989f3180a497a87
Tony <lin1000@gmail.com>
2017-10-09 14:21:09 +0800

    add today's git and parameterized headline

--- node/server.js
+++ node/server.js
@@ -9,6 +9,10 @@ var cookieParser = require('cookie-parser')
 result =  require('dotenv').config()
 console.log(result.parsed)
 
+//Get Mapbox API Key from Env
+var mapboxApiKey = process.env.mapbox_api_key;
+  
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -22,19 +26,22 @@ app.use(cookieParser())
 // index page 
 app.get('/', function (req, res) {
 
-  //Get Mapbox API Key from Env
-  var mapboxApiKey = process.env.mapbox_api_key;
-  
   var drinks = [
     { name: 'Bloody Mary', drunkness: 3 },
     { name: 'Martini', drunkness: 5 },
     { name: 'Scotch', drunkness: 10 }
   ];
-  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  //Determin which city to be displyed
+  cityName = "Taipei";
 
   res.render('pages/index', {
     drinks: drinks,
-    tagline: tagline,
+    city : {
+      cityHeadLine : "What's Happened in " + cityName + " Today",
+      cityName : "Taipei",
+      cityEventSummary : "To be composed automatically using algorithm."
+    },
     mapboxApiKey: mapboxApiKey
   });
 
@@ -47,17 +54,20 @@ app.get('/about', function (req, res) {
 
 app.get('/tpx.gif', function (req, res, next) {
 
-  console.log("Cookies: ", req.cookies)
+  console.log("Cookies: ", req.cookies);
+  console.log("next: " , next);
+
 
   io.emit('visit', {
     ip: req.ip,
+    reqheaders: req.headers,
     ua: req.headers['user-agent']
   });
 
   emptygif.sendEmptyGif(req, res, {
-    'Content-Type': 'image/gif',
+    'Content-Type': 'image/png',
     'Content-Length': emptygif.emptyGifBufferLength,
-    'Cache-Control': 'public, max-age=0' // or specify expiry to make sure it will call everytime
+    'Cache-Control': 'public, max-age=0, expiry' // or specify expiry to make sure it will call everytime
   });
 });
 
